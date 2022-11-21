9150a41464a74564dde84fa3489a2f3da1f60236
b00m12 <noah.agboola17@gmail.com>
2022-11-20 21:09:11 -0500

    adding files to git

---
+++ main.py
@@ -0,0 +1,77 @@
+# Task-3 Code here:
+from datetime import datetime
+
+year = []
+month = []
+day = []
+date = []
+cdsaEntry=[]
+masterDate = []
+with open ("pharmacy_list.txt",'r') as f:
+    for line in f:
+        if "No" in line:
+            continue
+        else:
+            cdsaEntry.append(line.split(','))
+
+
+"""
+The Isoformat() function is used to 
+return a string of date, time, and UTC offset to the corresponding time zone in ISO 8601 format
+I used this so that I do not have to index the string manually and remove the dashes in between
+the month day and the year
+"""
+cdsaEntry.sort(key=lambda entry: datetime.fromisoformat(entry[2][1:11]))
+
+print("{:<20} | {:<15} | {:<20} | {:<15}".format('Patient Name', 'Drug ID', 'Prescription Date', 'Name of CDSA'))
+print(80 * '-')
+
+for entry in cdsaEntry:
+    print("{:<20} | {:<15} | {:<20} | {:<15}".format(entry[0], entry[1], entry[2], entry[4]))
+
+opium = 0
+hydrocone = 0
+etorphine = 0
+metopon = 0
+morphine = 0
+ethylmorphine = 0
+apomorphine = 0
+nicocodine = 0
+oxycodone = 0
+pethidine = 0
+for entry in cdsaEntry:
+    if " Opium" in entry:
+        opium = opium + 1
+    if " Hydrocone" in entry:
+        hydrocone += 1
+    if " Etorphine" in entry:
+        etorphine += 1
+    if " Metopon" in entry:
+        metopon += 1
+    if " Morphine" in entry:
+        morphine += 1
+    if " Ethylmorphine" in entry:
+        ethylmorphine += 1
+    if " Apomorphine" in entry:
+        apomorphine += 1
+    if " Nicocodine" in entry:
+        nicocodine += 1
+    if " Oxycodone" in entry:
+        oxycodone += 1
+    if " Pethidine" in entry:
+        pethidine += 1
+
+substance = 0
+substance = len(cdsaEntry)
+
+print("\nTotal Prescriptions with CDSA substances :", substance)
+print("Opium:", opium)
+print("Hydrocone:", hydrocone)
+print("Etorphine:", etorphine)
+print("Metopon:", metopon)
+print("Morphine:", morphine)
+print("Ethylmorphine:", ethylmorphine)
+print("Apomorphine:", apomorphine)
+print("Nicocodine:", nicocodine)
+print("Oxycodone:", oxycodone)
+print("Pethidine:", pethidine)
